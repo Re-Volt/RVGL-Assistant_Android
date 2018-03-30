aab248dece06d5bcc7180a58bf5e68dc6aad23c2
Tavisco <otavio.marpin@gmail.com>
2018-03-30 16:22:31 -0300

    Add: Unzipping on another thread

--- app/src/main/java/io/github/tavisco/rvglassistant/InstallActivity.java
+++ app/src/main/java/io/github/tavisco/rvglassistant/InstallActivity.java
@@ -1,14 +1,18 @@
 package io.github.tavisco.rvglassistant;
 
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.app.Activity;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
+import com.afollestad.materialdialogs.MaterialDialog;
 
 import org.apache.commons.compress.archivers.sevenz.SevenZArchiveEntry;
 import org.apache.commons.compress.archivers.sevenz.SevenZFile;
@@ -20,68 +24,123 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class InstallActivity extends Activity {
+public class InstallActivity extends AppCompatActivity {
+
+    Intent pIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_install);
 
-        Intent intent = getIntent();
-        String action = intent.getAction();
+        pIntent = getIntent();
+    }
 
-        if (action.compareTo(Intent.ACTION_VIEW) == 0) {
-            String scheme = intent.getScheme();
-            ContentResolver resolver = getContentResolver();
+    @Override
+    public void onResume(){
+        super.onResume();
 
-            if (scheme.compareTo(ContentResolver.SCHEME_CONTENT) == 0) {
-                Uri uri = intent.getData();
-                String name = getContentName(resolver, uri);
-                String assistFolder = Environment.getExternalStorageDirectory().toString() + File.separator + "RVGLAssist";
-                String importfilepath =  assistFolder + File.separator + name;
+        UnzipFile unzip = new UnzipFile(InstallActivity.this);
 
-                Log.v(">>>" , "Content intent detected: " + action + " : " + intent.getDataString() + " : " + intent.getType() + " : " + uri);
-                InputStream input = null;
-                try {
-                    File pasta = new File(assistFolder);
-                    if (!pasta.exists() && !pasta.isDirectory())
-                        if (pasta.mkdir())
-                            Log.d(">>>", "Folders created");
+        unzip.execute(pIntent);
+    }
 
-                    input = resolver.openInputStream(uri);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
+    private class UnzipFile extends AsyncTask<Intent, String, String> {
 
-                InputStreamToFile(input, importfilepath);
+        private MaterialDialog dialog;
+        private Context mContext;
 
-                File file = new File(importfilepath);
+        public UnzipFile(Context context){
+            mContext = context;
+        }
 
-                try {
-                    SevenZFile sevenZFile = new SevenZFile(file);
-                    SevenZArchiveEntry entry;
-                    while ((entry = sevenZFile.getNextEntry()) != null){
-                        if (entry.isDirectory()){
-                            continue;
+
+        @Override
+        protected String doInBackground(Intent... mIntent) {
+            Intent intent = mIntent[0];
+            String action = intent.getAction();
+
+            if (action.compareTo(Intent.ACTION_VIEW) == 0) {
+                String scheme = intent.getScheme();
+                ContentResolver resolver = getContentResolver();
+
+                if (scheme.compareTo(ContentResolver.SCHEME_CONTENT) == 0) {
+                    Uri uri = intent.getData();
+                    String name = getContentName(resolver, uri);
+                    String assistFolder = Environment.getExternalStorageDirectory().toString() + File.separator + "RVGLAssist";
+                    String importfilepath =  assistFolder + File.separator + name;
+
+                    InputStream input = null;
+                    try {
+                        File folder = new File(assistFolder);
+                        if (!folder.exists() && !folder.isDirectory())
+                            folder.mkdir();
+
+                        if (uri != null) {
+                            input = resolver.openInputStream(uri);
                         }
-                        File curfile = new File(assistFolder + File.separator + "unzipped" + File.separator, entry.getName());
-                        File parent = curfile.getParentFile();
-                        if (!parent.exists()) {
-                            parent.mkdirs();
-                        }
-                        FileOutputStream out = new FileOutputStream(curfile);
-                        byte[] content = new byte[(int) entry.getSize()];
-                        sevenZFile.read(content, 0, content.length);
-                        out.write(content);
-                        out.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
 
+                    InputStreamToFile(input, importfilepath);
+
+                    File file = new File(importfilepath);
+
+                    try {
+                        SevenZFile sevenZFile = new SevenZFile(file);
+                        SevenZArchiveEntry entry;
+                        while ((entry = sevenZFile.getNextEntry()) != null){
+                            if (entry.isDirectory()){
+                                continue;
+                            }
+
+                            String fileName = entry.getName();
+                            publishProgress(fileName);
+
+                            File curfile = new File(assistFolder + File.separator + "unzipped" + File.separator, fileName);
+                            File parent = curfile.getParentFile();
+                            if (!parent.exists()) {
+                                parent.mkdirs();
+                            }
+                            FileOutputStream out = new FileOutputStream(curfile);
+                            byte[] content = new byte[(int) entry.getSize()];
+                            sevenZFile.read(content, 0, content.length);
+                            out.write(content);
+                            out.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
 
             }
 
+            return "ok";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            dialog.dismiss();
+
+            //Manipular View aqui
+
+
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+            dialog = new MaterialDialog.Builder(mContext)
+                    .title("Unzipping file")
+                    .content("Start unzipping")
+                    .progress(true, 0)
+                    .show();
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            dialog.setContent(values[0]);
         }
 
     }
